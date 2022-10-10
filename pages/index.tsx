6bd7e45d143f3d738c9fdf643c4bbb8d3105f88d
Parker Brown <brownpt98@gmail.com>
2022-10-09 20:23:24 -0500

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Steam Deck Guides, Compatibility, & More - DeckHub</title>
+                <meta name="description" content="Your place for all things Steam Deck" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <div className="grid place-items-center min-h-screen">
+                <h1 className="font-bold text-8xl">Coming Soon!</h1>
+            </div>
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}"
+  ],
+  theme: {
+    fontFamily: {
+      "sans": "Poppins, Arial, sans-serif"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
